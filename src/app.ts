65b4a4440aa17e01fbb9afc6d93ef2671e3ffb5d
Rohan Mia <rohanmia.org@gmail.com>
2026-07-15 14:20:28 +0600

    feat: add server listen call to start Express server

--- src/app.ts
+++ src/app.ts
@@ -26,4 +26,8 @@ app.use("/api/invitations", invitationRoutes);
 
 app.use(errorHandler);
 
+app.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`);
+});
+
 export default app;
